b8cc104f065d0b04773c9eaeae7f377904ad3a8e
Sathya Prabha Girish <sathya.prabha.girish@sap.com>
2020-07-01 12:24:26 -0400

    use babel to build transpiled js

---
+++ build/App.js
@@ -0,0 +1,18 @@
+import assignPlayerName from "./player";
+import { printGame, calculateScore, getProblemCount } from "./game";
+
+console.log("App self start with systemJS");
+
+// add click handler to the start game button
+document.getElementById('startGame').addEventListener('click', function () {
+    assignPlayerName(document.getElementById('playername').value); // this works because setName is the default exported object
+    printGame();
+});
+
+// add click handler to the calculate score button
+document.getElementById('calculate').addEventListener('click', function () {
+    calculateScore();
+});
+
+// set the default number of problems
+document.getElementById('problemCount').value = getProblemCount();
---
+++ build/player.js
@@ -0,0 +1,17 @@
+//private members here.
+var playerName = '';
+
+export function logPlayer() {
+    console.log('The current player is ' + playerName + '.');
+}
+
+export default function setName(newName) {
+    playerName = newName;
+}
+
+export function getName() {
+    return playerName;
+}
+
+// alternatively, expose through ex2015 export object
+// export {logPlayer, setName, getName};
